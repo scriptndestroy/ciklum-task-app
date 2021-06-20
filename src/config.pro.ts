7dc042f4790c13360c89c8461ece1187c634f81e
scriptndestroy <ojedamaruny@gmail.com>
2021-06-20 19:50:40 +0200

    🚀 Deploy

--- src/config.pro.ts
+++ src/config.pro.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    ciklum_api:"https://localhost:5001/",
+    // ciklum_api:"https://localhost:5001/",
+    ciklum_api:"https://localhost:44311/",
     DEV:false
 };
